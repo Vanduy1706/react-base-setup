6ddb7392ceefaca9d4ef16e94518d19a1b119b45
Vo Van Duy <vovanduy170603@gmail.com>
2025-07-14 09:57:07 +0700

    Update react runtime to not have to import react

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
 import App from "./App";
 import "./style.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
